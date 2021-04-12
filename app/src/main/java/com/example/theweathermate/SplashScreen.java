5d3d947b227daa99c3ea4910cf6a74006d52e2c8
Deven <devenwarang4@gmail.com>
2021-04-12 19:47:44 +0530

    Retrieving Location Bug Fixed

--- app/src/main/java/com/example/theweathermate/SplashScreen.java
+++ app/src/main/java/com/example/theweathermate/SplashScreen.java
@@ -2,14 +2,20 @@ package com.example.theweathermate;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.location.LocationManagerCompat;
 
 import com.example.theweathermate.ModelClasses.CurrentLocation;
 import com.example.theweathermate.Utils.AlertDialogBox;
@@ -20,25 +26,54 @@ import com.example.theweathermate.Utils.Permissions;
 
 public class SplashScreen extends AppCompatActivity implements DeviceLocation.locationListener {
 
-    private static final String TAG = "SPLASH_SCREEN";
+    private static final String TAG = "mTAG";
+    private static final int GPS_REQUEST_CODE = 10;
+
     TextView fetchingLocation;
+    DeviceLocation deviceLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        deviceLocation = new DeviceLocation();
         fetchingLocation = findViewById(R.id.textView);
 
         HelperMethods.setBlinkingText(fetchingLocation);
 
-        if (Permissions.isPermissionsGranted(this))
-            new DeviceLocation().getDeviceLocation(this, this);
+        getDeviceLocation();
+    }
 
-        else
+    private void getDeviceLocation() {
+        if (Permissions.isPermissionsGranted(this)) {
+            if (isGPSEnabled())
+                deviceLocation.getDeviceLocation(this, this);
+            else
+                showAlertDialog();
+        } else
             ActivityCompat.requestPermissions(this, Permissions.PERMISSIONS, Permissions.PERMISSION_CODE);
     }
 
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        return LocationManagerCompat.isLocationEnabled(locationManager);
+    }
+
+
+    private void showAlertDialog() {
+
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+        alBuilder.setTitle("GPS Permission");
+        alBuilder.setMessage("GPS is required for this app to work. Please enable GPS.");
+        alBuilder.setPositiveButton("Yes", (dialog, which) -> {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, GPS_REQUEST_CODE);
+        });
+        alBuilder.setCancelable(false);
+        alBuilder.create().show();
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -53,11 +88,20 @@ public class SplashScreen extends AppCompatActivity implements DeviceLocation.lo
         }
 
         if (flag)
-            new DeviceLocation().getDeviceLocation(this, this);
+            getDeviceLocation();
         else
             openMainActivity();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GPS_REQUEST_CODE && isGPSEnabled()) {
+            Log.d(TAG, "onActivityResult: BC");
+            getDeviceLocation();
+        } else
+            showAlertDialog();
+    }
 
     private void openMainActivity() {
         Handler handler = new Handler(getMainLooper());
@@ -65,7 +109,7 @@ public class SplashScreen extends AppCompatActivity implements DeviceLocation.lo
             Intent intent = new Intent(SplashScreen.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
-        },1000);
+        }, 1000);
     }
 
 
@@ -87,17 +131,12 @@ public class SplashScreen extends AppCompatActivity implements DeviceLocation.lo
             intent.putExtra(Constants.CURRENT_LOCATION, currentLocation);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
-        }, 2000);
+        }, 1000);
     }
 
     @Override
     public void onTaskFailed(String exception) {
         HelperMethods.showToastInCenter(this, exception).setDuration(Toast.LENGTH_LONG);
-        Handler handler = new Handler(getMainLooper());
-        handler.postDelayed(() -> {
-            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }, 2000);
+        openMainActivity();
     }
 }
--- app/src/main/java/com/example/theweathermate/Utils/DeviceLocation.java
+++ app/src/main/java/com/example/theweathermate/Utils/DeviceLocation.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Location;
+import android.os.CountDownTimer;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -12,6 +15,7 @@ import androidx.core.app.ActivityCompat;
 
 import com.example.theweathermate.ModelClasses.CurrentLocation;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
@@ -19,31 +23,40 @@ import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class DeviceLocation {
 
+    public static final String TAG = "mTAG";
+    public static final long START_TIME = 8000;
+    public static long TIMER_MILLIS = START_TIME;
+
+    Context context;
+    CountDownTimer countDownTimer;
+    LocationCallback locationCallback;
     locationListener locationListener;
+    FusedLocationProviderClient locationProviderClient;
 
     public void getDeviceLocation(Context context, locationListener locationListener) {
 
+        startTimer();
+        this.context = context;
         this.locationListener = locationListener;
-        FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(context);
 
-        LocationCallback locationCallback = new LocationCallback() {
+        locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
 
-                new FetchAddress(context, new FetchAddress.workerThreadListener() {
-                    @Override
-                    public void onTaskCompleted(Address address) {
-                        getAddressLine(address);
-                    }
+                Log.d(TAG, "onLocationResult: " + locationResult.getLastLocation().getLongitude());
+                stopTimer();
+                // Reverse GeoCoding
+                startWorkerThread(locationResult.getLastLocation());
+            }
 
-                    @Override
-                    public void onTaskFailed(String exception) {
-                        locationListener.onTaskFailed(exception);
-                    }
-                }).execute(locationResult.getLastLocation());
+            @Override
+            public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                Log.d(TAG, "onLocationAvailability: " + locationAvailability);
             }
         };
 
@@ -58,15 +71,73 @@ public class DeviceLocation {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationProviderClient.requestLocationUpdates(getLocationRequest(), locationCallback, null);
+        locationProviderClient.requestLocationUpdates(getLocationRequest(), locationCallback, Looper.getMainLooper());
+    }
+
+    private void stopTimer() {
+        countDownTimer.cancel();
+        TIMER_MILLIS = START_TIME;
+    }
+
+
+    private void startTimer() {
+
+        countDownTimer = new CountDownTimer(TIMER_MILLIS, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                TIMER_MILLIS = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d(TAG, "onFinish: timerFinished");
+                TIMER_MILLIS = START_TIME;
+                removeLocationUpdates();
+                if (context != null) {
+                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                                    == PackageManager.PERMISSION_GRANTED) {
+                        locationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Location location = task.getResult();
+                                if (location != null) {
+                                    //Reverse Geo Coding
+                                    startWorkerThread(location);
+                                } else
+                                    locationListener.onTaskFailed("Unable to get Location." +
+                                            " Error: location object is null");
+                            } else
+                                locationListener.onTaskFailed("Unable to get Location." +
+                                        " Error: " + Objects.requireNonNull(task.getException()).getMessage() + ".");
+                        });
+                    }
+                }
+            }
+        };
+        countDownTimer.start();
+    }
+
+    private void startWorkerThread(Location location) {
+        new FetchAddress(context, new FetchAddress.workerThreadListener() {
+            @Override
+            public void onTaskCompleted(Address address) {
+                Log.d(TAG, "onTaskCompleted: " + address);
+                getAddressLine(address);
+            }
+
+            @Override
+            public void onTaskFailed(String exception) {
+                locationListener.onTaskFailed(exception);
+            }
+        }).execute(location);
     }
 
 
     private void getAddressLine(Address address) {
 
         List<String> addressParts = new ArrayList<>();
-        if(address!=null) {
-
+        if (address != null) {
             for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
                 addressParts.add(address.getAddressLine(i));
             }
@@ -83,7 +154,13 @@ public class DeviceLocation {
 
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
         return locationRequest;
+    }
+
+    public void removeLocationUpdates() {
+        locationProviderClient.removeLocationUpdates(locationCallback);
     }
 
 
